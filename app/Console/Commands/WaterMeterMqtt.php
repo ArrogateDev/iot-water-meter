61ed4ef2b2303da9693810d1b55a2ec9ff1ec1c0
ARROGATE-MENG\novms <dev@arrogate.hk>
2025-06-21 13:45:47 +0800

    调整

---
+++ app/Console/Commands/WaterMeterMqtt.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class WaterMeterMqtt extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:water-meter-mqtt';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Log::info("water meter mqtt start");
+        Log::info("water meter mqtt:" . time());
+        while (true) {
+            if (random_int(1, 1000) > 990) {
+                throw new \Exception("water meter mqtt error");
+            }
+        }
+    }
+}
